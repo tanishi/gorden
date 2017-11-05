559258d23ba48d4b4e75f3db515afafed629943c
tanishi <tanishi503@gmail.com>
2017-11-05 17:02:24 +0900

    Add base tool

---
+++ cli.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	ExitCodeOK = iota
+	ExitCodeParseFlagError
+)
+
+type CLI struct {
+	outStream io.Writer
+	errStream io.Writer
+}
+
+func (c *CLI) Run(args []string) int {
+	var userID string
+
+	flags := flag.NewFlagSet("gorden", flag.ContinueOnError)
+	flags.SetOutput(c.errStream)
+	flags.StringVar(&userID, "userID", "", "github account")
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeParseFlagError
+	}
+
+	if userID == "" {
+		userID = os.Getenv("GIT_COMMITTER_NAME")
+	}
+
+	if userID == "" {
+		flags.Usage()
+		return ExitCodeParseFlagError
+	}
+
+	url := "https://github.com/users/" + userID + "/contributions"
+
+	contributions, sum := GetContributions(url)
+
+	fmt.Fprintf(c.outStream, "Current Streak: %d\n", contributions)
+	fmt.Fprintf(c.outStream, "Year of contributions: %d\n", sum)
+
+	return ExitCodeOK
+}
+
+func GetContributions(url string) (int, int) {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var contributions, sum int
+
+	doc.Find("rect").Each(func(_ int, s *goquery.Selection) {
+		datacount, _ := s.Attr("data-count")
+		c, _ := strconv.Atoi(datacount)
+
+		if c > 0 {
+			contributions++
+		} else {
+			contributions = 0
+		}
+
+		sum += c
+	})
+
+	return contributions, sum
+}
